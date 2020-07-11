1a2bbeaaf75949eb8c915d90e689a9c589fc0238
herypank <60302361+herypank@users.noreply.github.com>
2020-07-11 22:44:37 +0300

    Add files via upload

--- devman.py
+++ devman.py
@@ -1,8 +1,17 @@
 import requests
+
 cities = ['Лондон','Шереметьево','Череповец']
+payload = {
+    'n':'',
+    'T':'',
+    'q':'',
+    'u':'',
+    'm':'',
+    '&lang':'ru'}
+
 
 for city in cities:
-    url = 'http://wttr.in/{}?nTqum&lang=ru'.format(city)
+    url = 'http://wttr.in/{}'.format(city)
     response = requests.get(url, params=payload)
     response.raise_for_status()
     print(response.text)
